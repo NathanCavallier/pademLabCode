19cd5142c67d18f0963a60adbc7bb05ca40b238c
Nathan Imogo <nathan.imogo@outlook.fr>
2024-05-28 13:50:23 +0200

    modified:   dashboard/display_projects.html new file:   functions.php

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php 
+function enqueue_custom_script() {
+    if (is_user_logged_in()) {
+        $current_user = wp_get_current_user();
+        if ($current_user && isset($current_user->user_email) && !empty($current_user->user_email)) {
+            $user_email = $current_user->user_email;
+        } else {
+            $user_email = 'No email found';
+        }
+    } else {
+        $user_email = 'Not logged in';
+    }
+
+    // Enregistrer un script inline dummy
+    wp_register_script('custom-inline-script', '');
+    wp_enqueue_script('custom-inline-script');
+    wp_localize_script('custom-inline-script', 'user_data', array('email' => $user_email));
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_script');
+
+
+?>
