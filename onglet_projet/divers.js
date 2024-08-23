938a7266724c0507f133c6a18fb32d9cf90a1125
Nathan Imogo <nathan.imogo@outlook.fr>
2024-08-23 11:39:19 +0200

    new file:   .hintrc modified:   onglet_projet/display_project.html modified:   onglet_projet/divers.js

--- onglet_projet/divers.js
+++ onglet_projet/divers.js
@@ -80,3 +80,88 @@ try {
 } catch (error) {
     console.error('Erreur lors de l\'ajout de l\'évènement pour envoyer le dossier:', error);
 }
+
+
+try {
+    // Envoyer la trame
+    document.getElementById('onglet_trame_send').addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // Récupérer le premier fichier
+        let trame_file = document.getElementById('trame_file_1').files[0];
+        let trame_file_name = trame_file.name;
+        let trame_file_type = trame_file.type;
+        let trame_file_size = trame_file.size;
+
+        // Récupérer l'adresse email de l'utilisateur connecté
+        let userEmail = current_user_data.email;
+
+        // Vérifier si les champs sont vides
+        if (trame_file.length === 0) {
+            alert('Veuillez ajouter un fichier.');
+            return;
+        }
+
+        // Afficher l'avancement du chargement
+        document.getElementById('loading').style.display = 'block';
+
+        // Créer un objet FormData
+        let formData = new FormData();
+        formData.append('file', trame_file);
+        formData.append('action', 'handle_file_upload');
+
+        // Envoyer le fichier
+        fetch(ajaxurl, {
+                method: 'POST',
+                body: formData,
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    //#region Envoi des données de la trame
+                    fetch('https://lab.padem.org/wp-json/mo/v1/createProjectFile', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            body: JSON.stringify({
+                                file_name: ((data.data.url).split('/')).pop(), // Nom du fichier
+                                file_type: trame_file_type,
+                                file_link: data.data.url, // Lien du fichier
+                                file_size: trame_file_size,
+                                author_email: userEmail,
+                                is_trame: true,
+                                created_at: new Date(Date.now()) // Date de création
+                            }),
+                        })
+                        .then((response) => response.json())
+                        .then((data) => {
+                            // Masquer l'avancement du chargement
+                            document.getElementById('loading').style.display = 'none';
+                            // Recharger les trames
+                            displayTrames();
+
+                            // Vider les champs du formulaire
+                            document.getElementById('trame_file_1').value = '';
+                        })
+                        .catch((error) => {
+                            console.error('Error:', error);
+                            // Masquer l'avancement du chargement
+                            document.getElementById('loading').style.display = 'none';
+                        });
+                    //#endregion
+                } else {
+                    console.log('Error:', data.data);
+                    // Masquer l'avancement du chargement
+                    document.getElementById('loading').style.display = 'none';
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Masquer l'avancement du chargement
+                document.getElementById('loading').style.display = 'none';
+            });
+    });
+} catch (error) {
+    console.error('Erreur lors de l\'ajout de l\'évènement pour envoyer la trame:', error);
+}
